da0c8441349e22930bfa1dcd0232bc5023ecdcf2
pcourteille <pierre.courteille@gmail.com>
2025-05-20 10:41:26 +0200

    fix error vde stagin no comma

--- app/core/config.py
+++ app/core/config.py
@@ -40,7 +40,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
-    ENVIRONMENT: Literal["local", "dev" "staging", "prod"] = "local"
+    ENVIRONMENT: Literal["local", "dev", "staging", "prod"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
